1a3a69f86fb45f97afa5a14d279b89221956001d
JoelBeau <joelbravo@protonmail.com>
2024-08-29 18:20:40 -0500

    Updated bash script to run air2cap.py after raw2air.py

--- sensor-captions-app/scripts/process_raw_files.sh
+++ sensor-captions-app/scripts/process_raw_files.sh
@@ -4,10 +4,17 @@
 BASE_DIR="../data"
 
 # Define the Python script to run
-PYTHON_SCRIPT="../scripts/raw2air.py"
+RAW2_AIR_PYTHON_SCRIPT="../scripts/raw2air.py"
+AIR2_CAP_PYTHON_SCRIPT="../scripts/air2cap.py"
 
-# Find all raw.csv files in the data directory and its subdirectories
+# Find all raw.csv files in the data directory and its subdirectories, and run the Python script on each file
 find "$BASE_DIR" -type f -name "raw.csv" | while read -r raw_file; do
     # Run the Python script on each raw.csv file
-    python "$PYTHON_SCRIPT" "$raw_file"
+    python "$RAW2_AIR_PYTHON_SCRIPT" "$raw_file"
+done
+
+# Find all air.csv files in the data directory and its subdirectories, and run the Python script on each file to generate captions
+find "$BASE_DIR" -type f -name "air.csv" | while read -r air_file; do
+    # Run the Python script on each raw.csv file
+    python "$AIR2_CAP_PYTHON_SCRIPT" "$air_file"
 done
