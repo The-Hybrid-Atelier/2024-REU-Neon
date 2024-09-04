e978cd6e9eb4dea6f4cc6b03178bbd08399fe616
JoelBeau <joelbravo@protonmail.com>
2024-09-04 13:41:09 -0500

    Modified bash script to run air2meta script with all the meta types for each air.csv file in data/

--- sensor-captions-app/scripts/process_raw_files.sh
+++ sensor-captions-app/scripts/process_raw_files.sh
@@ -6,6 +6,9 @@ BASE_DIR="../data"
 # Define the Python script to run
 RAW2_AIR_PYTHON_SCRIPT="../scripts/raw2air.py"
 AIR2_CAP_PYTHON_SCRIPT="../scripts/air2cap.py"
+AIR2_META_PYTHON_SCRIPT="../scripts/air2meta.py"
+
+META_TYPES=("sound" "light" "synth" "vibration")
 
 # Find all raw.csv files in the data directory and its subdirectories, and run the Python script on each file
 find "$BASE_DIR" -type f -name "raw.csv" | while read -r raw_file; do
@@ -17,4 +20,7 @@ done
 find "$BASE_DIR" -type f -name "air.csv" | while read -r air_file; do
     # Run the Python script on each raw.csv file
     python "$AIR2_CAP_PYTHON_SCRIPT" "$air_file"
+    for i in "${META_TYPES[@]}"; do
+        python "$AIR2_META_PYTHON_SCRIPT" "$air_file" "$i"
+    done
 done
