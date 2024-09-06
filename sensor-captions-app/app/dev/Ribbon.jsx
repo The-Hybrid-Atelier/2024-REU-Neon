4a5ae4984ae470b9e4537b0390bcb9810316a638
SapphireDragonn <146882019+SapphireDragonn@users.noreply.github.com>
2024-09-06 01:41:33 +0000

    Minor Update to Ribbon Component

--- sensor-captions-app/app/dev/Ribbon.jsx
+++ sensor-captions-app/app/dev/Ribbon.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleNodes, faGoogle } from '@fortawesome/free-solid-svg-icons';
 
 const Ribbon = ({ icons, labels, isActive, onIconClick, typeSelect }) => {
   return (
