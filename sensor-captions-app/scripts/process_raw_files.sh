0ed2750514f97a50cb64d8c3d8be832e025d974c
JoelBeau <joelbravo@protonmail.com>
2024-09-10 11:31:29 -0500

    Updated bash script to try and exclude the test folder

--- sensor-captions-app/scripts/process_raw_files.sh
+++ sensor-captions-app/scripts/process_raw_files.sh
@@ -11,13 +11,13 @@ AIR2_META_PYTHON_SCRIPT="../scripts/air2meta.py"
 META_TYPES=("sound" "light" "synth" "vibration")
 
 # Find all raw.csv files in the data directory and its subdirectories, and run the Python script on each file
-find "$BASE_DIR" -type f -name "raw.csv" | while read -r raw_file; do
+find "$BASE_DIR" \( -type d -name "test" -o -type d -name "tests" \) -prune -o -type f -name "raw.csv" | while read -r raw_file; do
     # Run the Python script on each raw.csv file
     python "$RAW2_AIR_PYTHON_SCRIPT" "$raw_file"
 done
 
 # Find all air.csv files in the data directory and its subdirectories, and run the Python script on each file to generate captions
-find "$BASE_DIR" -type f -name "air.csv" | while read -r air_file; do
+find "$BASE_DIR" \( -type d -name "test" -o -type d -name "tests" \) -prune -o -type f -name "air.csv" | while read -r air_file; do
     # Run the Python script on each raw.csv file
     python "$AIR2_CAP_PYTHON_SCRIPT" "$air_file"
     for i in "${META_TYPES[@]}"; do
